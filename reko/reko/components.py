9f67cba2a1e327411f91b1acebf44d265ca83486
Andreas Pelme <andreas@pelme.se>
2024-06-15 15:15:55 +0200

    shoelace->pico.css

--- reko/reko/components.py
+++ reko/reko/components.py
@@ -1,4 +1,5 @@
 import htpy as h
+from django.forms import BoundField
 from django.http import HttpRequest
 from django.middleware.csrf import get_token
 from django.template.backends.utils import csrf_input
@@ -12,7 +13,7 @@ from .models import Order, Producer
 
 
 def base(*, request: HttpRequest, title: str, logo_url: str, content: h.Node, cart: h.Node = None) -> h.Element:
-    return h.html(".sl-theme-light", lang="sv")[
+    return h.html(lang="sv")[
         h.head[
             h.meta(charset="utf-8"),
             h.meta(
@@ -31,25 +32,17 @@ def base(*, request: HttpRequest, title: str, logo_url: str, content: h.Node, ca
             h.script(defer=True, src=static("vendor/htmx.min.js")),
             h.link(
                 rel="stylesheet",
-                href="https://cdn.jsdelivr.net/npm/@shoelace-style/shoelace@2.15.0/cdn/themes/light.css",
-            ),
-            h.link(
-                rel="stylesheet",
-                href="https://cdn.jsdelivr.net/npm/@shoelace-style/shoelace@2.15.0/cdn/themes/dark.css",
-            ),
-            h.script(
-                type="module",
-                src="https://cdn.jsdelivr.net/npm/@shoelace-style/shoelace@2.15.0/cdn/shoelace-autoloader.js",
+                href=static("vendor/pico.green.min.css"),
             ),
         ],
         h.body(hx_headers='{"X-CSRFToken": "%s"}' % get_token(request))[
             h.header[
-                h.div(".content")[
+                h.div(".container")[
                     h.a(href=logo_url)[h.img(".logo", src=static("reko/logo.png"), alt="Rekoplus logo")],
                     cart,
                 ],
             ],
-            h.main[content],
+            h.main(".container")[content],
             h.footer[
                 h.ul[
                     h.a(href="/")[h.li["Om rekoplus.se"]],
@@ -71,7 +64,7 @@ def producer_base(
         logo_url=reverse("producer-index", args=[producer.slug]),
         cart=(
             h.a(".cart", href=reverse("order", args=[producer.slug]))[
-                h.sl_icon(name="bag", label="Varukorg"),
+                basket_icon(),
                 f"{cart.total_count()} varor, {format_price(cart.total_price())}",
             ]
             if cart
@@ -80,46 +73,44 @@ def producer_base(
     )
 
 
-def cart_update_button(*, icon: str, label: str, action: str) -> h.Element:
-    return h.sl_tooltip(content=label)[
-        h.sl_button(type="submit", name="plus", value="1", circle=True)[h.sl_icon(name=icon)]
-    ]
-
-
 def product_card(url: str, form: ProductCartForm) -> h.Element:
     product = form.product
 
     current_count = form.initial.get("count", 0)
 
-    return h.sl_card(".product")[
-        h.img(slot="image", loading="lazy", src=str(product.card_thumbnail.url), alt=product.name, height="150"),
-        h.div(".name-and-price")[
-            h.a(href="#")[h.h3[product.name]],
-            h.span[format_price(product.price)],
-        ],
-        h.p(".description")[product.description],
-        h.form(hx_post=url, hx_trigger="sl-change,submit,change", hx_target="body")[
-            (
-                form["plus"].as_widget(
-                    SubmitWidget(
-                        [
-                            h.sl_icon(
-                                name="plus-lg",
-                                slot="prefix",
-                            ),
-                            "Lägg till i varukorg",
-                        ],
-                        class_="buy",
-                    )
-                ),
-            )
-            if not current_count
-            else [
-                h.sl_tooltip(content="Ta bort en vara")[form["minus"].as_widget(SubmitWidget("-", pill=True))],
-                form["count"],
-                h.sl_tooltip(content="Lägg till ytterligare")[form["plus"].as_widget(SubmitWidget("+", pill=True))],
-            ]
-        ],
+    return h.a(href="#")[
+        h.article(".product")[
+            h.header[
+                h.figure(style=("border-radius: var(--pico-border-radius);" "overflow: hidden;"))[
+                    h.img(
+                        slot="image",
+                        loading="lazy",
+                        src=str(product.card_thumbnail.url),
+                        alt=product.name,
+                        height="150",
+                    ),
+                ],
+                h.h4[product.name],
+            ],
+            h.p(".description")[product.description],
+            h.footer[
+                h.span[format_price(product.price)],
+                h.form(
+                    role="group",
+                    hx_post=url,
+                    hx_trigger="submit,change",
+                    hx_target="body",
+                )[
+                    (form["plus"].as_widget(SubmitWidget("+ Lägg till", class_="buy")),)
+                    if not current_count
+                    else [
+                        form["minus"].as_widget(SubmitWidget("-")),
+                        h.span(".current-count")[str(current_count)],
+                        form["plus"].as_widget(SubmitWidget("+")),
+                    ]
+                ],
+            ],
+        ]
     ]
 
 
@@ -144,7 +135,7 @@ def producer_index(
                 ),
                 h.p[producer.description],
             ],
-            h.section(".products")[
+            h.section(".products-grid")[
                 (product_card(request.path, product_cart_form) for product_cart_form in product_cart_forms.forms)
             ],
         ),
@@ -170,7 +161,7 @@ def order(
             ],
             h.form(method="post")[
                 csrf_input(request),
-                h.table[
+                h.table(".striped")[
                     h.thead[
                         h.tr[
                             h.th["Produkt"],
@@ -196,14 +187,26 @@ def order(
                     h.h2["Dina uppgifter"],
                     h.ul[
                         h.div(".form")[
-                            order_form.as_ul(),
-                            h.li[h.button(type="submit", class_="submit")["Beställ!"]],
+                            _render_field(order_form["name"]),
+                            _render_field(order_form["email"]),
+                            _render_field(order_form["phone"]),
+                            _render_field(order_form["location"]),
+                            _render_field(order_form["note"]),
+                            h.button(type="submit", class_="submit")["Beställ!"],
                         ],
                     ],
                 ],
             ],
         ),
     )
+
+
+def _render_field(form_field: BoundField) -> h.Element:
+    return h.label[
+        form_field.label,
+        form_field,
+        h.div(".error-message")[form_field.errors],
+    ]
 
 
 def order_summary(*, request: HttpRequest, producer: Producer, order: Order) -> h.Element:
@@ -223,3 +226,19 @@ def order_summary(*, request: HttpRequest, producer: Producer, order: Order) -> 
             ],
         ],
     )
+
+
+def basket_icon() -> h.Element:
+    # https://icons.getbootstrap.com/icons/basket/
+    return h.svg(
+        ".bi.bi-basket",
+        xmlns="http://www.w3.org/2000/svg",
+        width="16",
+        height="16",
+        fill="currentColor",
+        viewbox="0 0 16 16",
+    )[
+        h.path(
+            d="M5.757 1.071a.5.5 0 0 1 .172.686L3.383 6h9.234L10.07 1.757a.5.5 0 1 1 .858-.514L13.783 6H15a1 1 0 0 1 1 1v1a1 1 0 0 1-1 1v4.5a2.5 2.5 0 0 1-2.5 2.5h-9A2.5 2.5 0 0 1 1 13.5V9a1 1 0 0 1-1-1V7a1 1 0 0 1 1-1h1.217L5.07 1.243a.5.5 0 0 1 .686-.172zM2 9v4.5A1.5 1.5 0 0 0 3.5 15h9a1.5 1.5 0 0 0 1.5-1.5V9zM1 7v1h14V7zm3 3a.5.5 0 0 1 .5.5v3a.5.5 0 0 1-1 0v-3A.5.5 0 0 1 4 10m2 0a.5.5 0 0 1 .5.5v3a.5.5 0 0 1-1 0v-3A.5.5 0 0 1 6 10m2 0a.5.5 0 0 1 .5.5v3a.5.5 0 0 1-1 0v-3A.5.5 0 0 1 8 10m2 0a.5.5 0 0 1 .5.5v3a.5.5 0 0 1-1 0v-3a.5.5 0 0 1 .5-.5m2 0a.5.5 0 0 1 .5.5v3a.5.5 0 0 1-1 0v-3a.5.5 0 0 1 .5-.5"  # noqa
+        )
+    ]
