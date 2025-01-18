f3b9b0344b9186ccc1a690d00e00fc1ad0b707dd
David Svenson <davidsvenson@outlook.com>
2025-01-18 22:55:45 +0100

    Add back to products button.

--- reko/reko/components.py
+++ reko/reko/components.py
@@ -172,6 +172,10 @@ def order(
                 h.h1[producer.display_name],
                 h.h2["Beställ dina varor"],
             ],
+            h.a(
+                style="align-self: flex-start;",
+                href=reverse("producer-index", args=[producer.slug]),
+            )[arrow_left_icon(style="margin-right: .4rem;"), "Tillbaka till produkter"],
             h.form(method="post")[
                 csrf_input(request),
                 h.table(
@@ -314,6 +318,24 @@ def basket_icon() -> h.Element:
     )[
         h.path(
             d="M5.757 1.071a.5.5 0 0 1 .172.686L3.383 6h9.234L10.07 1.757a.5.5 0 1 1 .858-.514L13.783 6H15a1 1 0 0 1 1 1v1a1 1 0 0 1-1 1v4.5a2.5 2.5 0 0 1-2.5 2.5h-9A2.5 2.5 0 0 1 1 13.5V9a1 1 0 0 1-1-1V7a1 1 0 0 1 1-1h1.217L5.07 1.243a.5.5 0 0 1 .686-.172zM2 9v4.5A1.5 1.5 0 0 0 3.5 15h9a1.5 1.5 0 0 0 1.5-1.5V9zM1 7v1h14V7zm3 3a.5.5 0 0 1 .5.5v3a.5.5 0 0 1-1 0v-3A.5.5 0 0 1 4 10m2 0a.5.5 0 0 1 .5.5v3a.5.5 0 0 1-1 0v-3A.5.5 0 0 1 6 10m2 0a.5.5 0 0 1 .5.5v3a.5.5 0 0 1-1 0v-3A.5.5 0 0 1 8 10m2 0a.5.5 0 0 1 .5.5v3a.5.5 0 0 1-1 0v-3a.5.5 0 0 1 .5-.5m2 0a.5.5 0 0 1 .5.5v3a.5.5 0 0 1-1 0v-3a.5.5 0 0 1 .5-.5"  # noqa: E501
+        )
+    ]
+
+
+def arrow_left_icon(style: str) -> h.Element:
+    # https://icons.getbootstrap.com/icons/arrow-left/
+    return h.svg(
+        ".bi.bi-arrow-left",
+        style=style,
+        xmlns="http://www.w3.org/2000/svg",
+        width="16",
+        height="16",
+        fill="currentColor",
+        viewBox="0 0 16 16",
+    )[
+        h.path(
+            fill_rule="evenodd",
+            d="M15 8a.5.5 0 0 0-.5-.5H2.707l3.147-3.146a.5.5 0 1 0-.708-.708l-4 4a.5.5 0 0 0 0 .708l4 4a.5.5 0 0 0 .708-.708L2.707 8.5H14.5A.5.5 0 0 0 15 8",  # noqa: E501
         )
     ]
 
