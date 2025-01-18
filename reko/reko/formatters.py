9203ea4abd0cd5b87ed4c533d5abbd32c490d4ef
David Svenson <davidsvenson@outlook.com>
2025-01-18 15:25:12 +0100

    Localize formatters.

--- reko/reko/formatters.py
+++ reko/reko/formatters.py
@@ -1,20 +1,17 @@
 from decimal import ROUND_UP, Decimal
 
+from django.utils.formats import localize
+
 
 def format_price(price: Decimal) -> str:
-    kronor = int(price)
-    ore = price % 1
-
-    ore_str = str(int(ore * 100)).zfill(2) if ore else "-"
-
-    return f"{kronor}:{ore_str}"
+    return f"{format_amount(price)} kr"
 
 
 def format_amount(amount: Decimal) -> str:
     # display with decimals if not a whole number
     if amount % 1:
-        return f"{amount:.2f}"
-    return str(int(amount))
+        return localize(quantize_decimal(amount))
+    return localize(int(amount))
 
 
 def quantize_decimal(number: Decimal) -> Decimal:
